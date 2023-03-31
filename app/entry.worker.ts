2d9474eb2f4a5caeb691c3e9b4917ddbf073bbed
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-31 13:07:23 +0100

    more verbose example for debug levels

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -9,7 +9,9 @@ const PAGES = 'page-cache-v1'
 const DATA = 'data-cache-v1'
 const ASSETS = 'assets-cache-v1'
 
-self.__DISABLE_PWA_DEV_LOGS = true;
+// Disables all logs in the console.
+// Check `worker/core.ts` for more granular control and log levels.
+// self.__DISABLE_PWA_DEV_LOGS = true;
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
