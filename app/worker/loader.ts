79b37e771e76524f77ecbb2bec40562c4313b894
Abdur-Rahman <abbadshaf05@gmail.com>
2023-04-01 12:02:00 +0100

    loader module

--- app/worker/loader.ts
+++ app/worker/loader.ts
@@ -2,13 +2,15 @@
  * Contains APIs pertaining to loading, and installing a Service Worker
  */
 
-import { logger } from "./core";
-import type { LoadServiceWorkerOptions } from "./types";
-
 /// <reference lib="WebWorker" />
 
 export type {};
 declare let self: ServiceWorkerGlobalScope;
+
+export type LoadServiceWorkerOptions = {
+  scope?: string;
+  serviceWorkerUrl?: string;
+};
 
 /**
  * Load service worker in `entry.client`
@@ -102,7 +104,7 @@ export function loadServiceWorker(
             // )
           );
       } catch (error) {
-        logger.error("Service worker registration failed", error);
+        // logger.error("Service worker registration failed", error);
       }
     }
 
