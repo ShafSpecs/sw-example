5f244e5fcb4940b14443a4c9880f72f1ac5081da
Abdur-Rahman <abbadshaf05@gmail.com>
2023-04-03 18:08:32 +0100

    rough implementation

--- app/worker/background-sync.ts
+++ app/worker/background-sync.ts
@@ -1,3 +1,20 @@
 // todo: Later please. Implementation wants to burst my head 
 // and I'm still figuring out how plugin would work, 
 // chaneged implemenation 4 times already.
+
+
+/**
+ * Rough implementation of background sync plugin.
+ * `registerBackgroundSync` is a function that registers the request,
+ * triggered by the `fetchDidFail` hook of the `StrategyPlugin` interface.
+ */
+// class BackgroundSyncPlugin implements Plugin {
+//   async fetchDidFail(request: Request, error: Error): void {
+//     await registerBackgroundSync({
+//       request: request,
+//       options: {
+//         tag: 'background-sync'
+//       }
+//     });
+//   }
+// }
